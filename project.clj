d244dcdc2b8ede46a41803b00fd4a52ec6c55ae9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-13 15:14:33 +0200

    Update deps to the latest clojure and midje deps.

--- project.clj
+++ project.clj
@@ -1,14 +1,8 @@
-(defproject denlab-4clojure-exercices/denlab-4clojure-exercices "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+(defproject sh-deps "1.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-stacktrace "0.2.4"]
-                 [org.clojure/tools.cli "0.2.1"]]
-  :profiles {:dev
-             {:dependencies
-              [[midje "1.4.0"]
-               [com.intelie/lazytest
-                "1.0.0-SNAPSHOT"
-                :exclusions
-                [swank-clojure]]]}}
+                 [org.clojure/tools.cli "0.2.2"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
   :main sh-deps.core
   :min-lein-version "2.0.0"
   :description "Shell script dependency graph analyzer")
